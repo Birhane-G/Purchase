50c8c678c5c227af89b10d94c2e041368e92e75c
Birhane Gebrial <birhanegebrial1@gmail.com>
2024-03-31 11:56:45 +0300

    App header

---
+++ Components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Header = () => {
+  return (
+    <View>
+        <Text>Purchasr Order</Text>
+    </View>
+  )
+}
+
+const style = StyleSheet.create({
+    conatiner: {
+
+    }
+})
+
+export default Header
